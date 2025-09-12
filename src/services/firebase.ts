4133311bf3fc192402a416837875cdde217d5193
Igor Vinicius <igor_vinicius.carvalho@somosicev.com>
2025-09-11 23:09:08 -0300

    first commit

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,21 +3,6 @@ import { getApps, initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
-// Configuração do Firebase para produção (original) - comentada
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyB2JMVHvH8FKs_GEl8JVRoRfPDjY9Ztcf8",
-  authDomain: "piauiticketsdb.firebaseapp.com",
-  databaseURL: "https://piauiticketsdb-default-rtdb.firebaseio.com",
-  projectId: "piauiticketsdb",
-  storageBucket: "piauiticketsdb.appspot.com",
-  messagingSenderId: "372256479753",
-  appId: "1:372256479753:web:8b5890e8c94dc75daaf6d8",
-  measurementId: "G-FMD1R115PG"
-};
-*/
-
-
 // Configuração do Firebase para desenvolvimento
 const firebaseConfig = {
   apiKey: "AIzaSyCdiQJBz7--aJMoCfh7HJ7_QV-KN1eidYQ",
